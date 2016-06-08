4de09ea57cec9c6f319d97daa0ae37ac7f37bcbc
Abigail Watson <awatson1978@gmail.com>
2016-06-08 12:29:11 -0500

    most schema fields are now optional

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-questionnaire-response',
-  version: '1.1.0',
+  version: '1.1.3',
   summary: 'HL7 FHIR Resource - Questionnaire Response',
   git: 'https://github.com/clinical-meteor/hl7-resource-questionnaire-response',
   documentation: 'README.md'
@@ -17,12 +17,13 @@ Package.onUse(function (api) {
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.4.0');
+  api.use('clinical:hl7-resource-datatypes@0.4.2');
 
   api.addFiles('lib/hl7-resource-questionnaire-response.js');
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
 
+  api.export('QuestionnaireResponse');
   api.export('QuestionnaireResponses');
   api.export('QuestionnaireResponseSchema');
 });
