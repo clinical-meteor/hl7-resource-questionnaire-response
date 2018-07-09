dcdcbc491297568a46150f03a3e8bff7326f11d2
Abigail Watson <abigailwatson@Abigails-Mac-mini-377974.local>
2018-07-09 01:08:57 -0500

    default to local collection (HIPAA compliant; but annoying to use)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-questionnaire-response',
-  version: '1.2.5',
+  version: '1.2.6',
   summary: 'HL7 FHIR Resource - Questionnaire Response',
   git: 'https://github.com/clinical-meteor/hl7-resource-questionnaire-response',
   documentation: 'README.md'
