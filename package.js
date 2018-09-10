59bdca2c61cc64881ad611a072f14d6a46bef764
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-09-09 20:59:37 -0500

    sync

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-questionnaire-response',
-  version: '1.3.1',
+  version: '1.3.2',
   summary: 'HL7 FHIR Resource - Questionnaire Response',
   git: 'https://github.com/clinical-meteor/hl7-resource-questionnaire-response',
   documentation: 'README.md'
@@ -49,5 +49,7 @@ Package.onUse(function (api) {
 });
 
 Npm.depends({
-  "simpl-schema": "1.5.3"
+  "simpl-schema": "1.5.3",
+  "moment": "2.22.2",
+  "lodash": "4.17.4"
 })
